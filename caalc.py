a24e88103fb792d103547cd15baef291ed7636e9
George V. Kouryachy (Fr. Br. George) <george@altlinux.ru>
2015-03-13 12:14:03 +0300

    TPG simple calc

--- caalc.py
+++ caalc.py
@@ -1,13 +1,38 @@
 #!/usr/bin/python
+# coding: utf
 
 import readline
 import sys
+import tpg
 
+def make_op(s):
+    return {
+        '+': lambda x,y: x+y,
+        '-': lambda x,y: x-y,
+        '*': lambda x,y: x*y,
+        '/': lambda x,y: x/y,
+    }[s]
+
+class Calc(tpg.Parser):
+    r"""
+
+    separator spaces: '\s+' ;
+
+    token number: '\d+' int ;
+    token add: '[+-]' make_op ;
+    token mul: '[*/]' make_op ;
+
+    START/e -> Term/e ;
+    Term/t -> Fact/t ( add/op Fact/f $t=op(t,f)$ )* ;
+    Fact/f -> Atom/f ( mul/op Atom/a $f=op(f,a)$ )* ;
+    Atom/a -> number/a | '\(' Term/a '\)' ;
+
+    """
+
+calc = Calc()
 PS1='--> '
 
 Stop=False
 while not Stop:
     line = raw_input(PS1)
-    print line
-
-
+    print calc(line)
