d97e6438432a7f771a0083091922e5b0268c4d40
Vysotsky Lev <vysotskylev@yandex.ru>
2015-03-24 12:01:44 +0300

    Replaced Vector with Matrix, implemented multiplication

--- caalc.py
+++ caalc.py
@@ -46,7 +46,49 @@ class Vector(list):
         except TypeError:
             return self.__class__(c or a for c in self)
 
-class Matrix:
+class Matrix(list):
+    def __init__(self, *argp, **argn):
+        list.__init__(self, *argp, **argn)
+        if len(self) != 0:
+            for row in self:
+                if len(row) != len(self[0]):
+                    raise TypeError("Rows must have equal lengths")
+            self.nrows = len(self)
+            self.ncols = len(self[0])
+        else:
+            self.nrows = self.ncols = 0
+    def __str__(self):
+        return "[" + " ".join(str(c) for c in self) + "]"
+
+    def __op(self, a, op):
+        try:
+            return self.__class__(op(s,e) for s,e in zip(self, a))
+        except TypeError:
+            return self.__class__(op(c,a) for c in self)
+
+    def __add__(self, a): return self.__op(a, lambda c,d: c+d)
+    def __sub__(self, a): return self.__op(a, lambda c,d: c-d)
+    def __mul__(self, a): 
+        if self.ncols != a.nrows:
+            raise TypeError("Inconsistent matrix sizes: {}x{} and {}x{}".format(self.nrows, self.ncols, a.nrows, a.ncols))
+        res = list(
+                list(
+                    sum(self[i][j] * a[j][k] for j in xrange(self.ncols)) 
+                    for k in xrange(a.ncols)
+                ) for i in xrange(self.nrows))
+        return self.__class__(res) 
+    def __and__(self, a):
+        try:
+            return reduce(lambda s, (c,d): s+c*d, zip(self, a), 0)
+        except TypeError:
+            return self.__class__(c and a for c in self)
+
+    def __or__(self, a):
+        try:
+            return self.__class__(itertools.chain(self, a))
+        except TypeError:
+            return self.__class__(c or a for c in self)
+
 class Calc(tpg.Parser):
     r"""
 
@@ -64,15 +106,13 @@ class Calc(tpg.Parser):
     Assign -> id/i '=' Expr/e $Vars[i]=e$ ;
     Expr/t -> Fact/t ( op1/op Fact/f $t=op(t,f)$ )* ;
     Fact/f -> Atom/f ( op2/op Atom/a $f=op(f,a)$ )* ;
-    Atom/a ->   Vector/a
-              | Matrix/a
+    Atom/a ->   Matrix/a
               | id/i ( check $i in Vars$ | error $"Undefined variable '{}'".format(i)$ ) $a=Vars[i]$
               | fnumber/a
               | number/a
               | '\(' Expr/a '\)' ;
-    Vector/$Vector(a)$ -> '\[' '\]' $a=[]$ | '\[' Atoms/a '\]' ;
-    Matrix/$Matrix(a)$ -> '\[' Lines/a '\]'
-    Lines/l -> Atoms/v ; Lines/l $l=[v]+l$ | Atoms/v $l=[v]$
+    Matrix/$Matrix(a)$ -> '\[' '\]' $a=[]$ | '\[' Lines/a '\]' ;
+    Lines/l -> Atoms/v ';' Lines/l $l=[v]+l$ | Atoms/v $l=[v]$ ;
     Atoms/v -> Atom/a Atoms/t $v=[a]+t$ | Atom/a $v=[a]$ ;
 
     """
